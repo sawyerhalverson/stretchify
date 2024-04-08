dc8c5d110ccb21db8f58428f0920c8f2aa80805e
sawyerhalverson <shalverson321@gmail.com>
2024-04-08 14:55:17 -0600

    Initial Commit

---
+++ stretchify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stretchify
+//
+//  Created by Sawyer Halverson on 4/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stretchify/stretchifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  stretchifyApp.swift
+//  stretchify
+//
+//  Created by Sawyer Halverson on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct stretchifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

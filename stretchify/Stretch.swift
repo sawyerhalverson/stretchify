c4dd5e2eb01e50afc17899cce97b3cd0a639aef5
sawyerhalverson <shalverson321@gmail.com>
2024-04-08 20:24:50 -0600

    trying json

---
+++ stretchify/Stretch.swift
@@ -0,0 +1,8 @@
+//
+//  Stretch.swift
+//  stretchify
+//
+//  Created by Sawyer Halverson on 4/8/24.
+//
+
+import Foundation
---
+++ stretchify/StretchesView.swift
@@ -0,0 +1,18 @@
+//
+//  StretchesView.swift
+//  stretchify
+//
+//  Created by Sawyer Halverson on 4/8/24.
+//
+
+import SwiftUI
+
+struct StretchesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StretchesView()
+}
